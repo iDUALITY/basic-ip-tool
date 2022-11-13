f31de2adae9100dc701de43a5fc27e1d8a32ef5a
dualityyz <74072831+iDUALITY@users.noreply.github.com>
2022-11-13 13:45:21 +0300

    Add files via upload

---
+++ ip.py
@@ -0,0 +1,10 @@
+import json
+from urllib.request import urlopen
+from tabulate import tabulate
+
+url = "https://ipinfo.io/json"
+response = urlopen (url)
+data = json.load(response)
+
+print(data)
+
